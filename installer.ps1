cf8fb1dcebc59175999cde9ad7444451b53e0122
Matt <33157232+LvInSaNevL@users.noreply.github.com>
2019-02-19 23:45:08 -0800

    User now has to install WSL themselves

--- installer.ps1
+++ installer.ps1
@@ -1,26 +1,29 @@
-# File references
+# You must run the script as admin
+#Requires -RunAsAdministrator
 
 # Global Variables              
 $start_time = Get-Date            
 $prog_loc = (Resolve-Path .\).Path
 $userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-# A simple method to allow us to reboot the computer in the middle of the script
-workflow Persistent_Reboot { Restart-Computer -Wait }
+$WSL = (Get-WindowsOptionalFeature -Online | where FeatureName -eq Microsoft-Windows-Subsystem-Linux)
+if ($WSL.State -eq "Disabled") {
+    Write-Host "Please install Windows Subsystem for Linux"
+    exit
+}
 
-# Enables WSL as a feature
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-Persistent_Reboot
 
 # Downloads the ubuntu client
+Write-Host "Downloading and extracting Ubuntu terminal"
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile Ubuntu.appx -UseBasicParsing
 Rename-Item ~/Ubuntu.appx ~/Ubuntu.zip
 Expand-Archive ~/Ubuntu.zip ~/Ubuntu
 
 # Adds the ubuntu application to your path
-[System.Environment]::SetEnvironmentVariable("PATH", $userenv + "C:\Users\Administrator\Ubuntu", "User")
+Write-Host "Adding Ubuntu to path"
+InlineScript { [System.Environment]::SetEnvironmentVariable("PATH", $userenv + "C:\Users\Administrator\Ubuntu", "User") }
 
 # Finally, restarts the computer so changes take effect
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-Write-Host "Restarting computer to save changes"
-Restart-Computer    
+Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+#Write-Host "Restarting computer to save changes"
+#Restart-Computer   

b11f92bcd710312cb946f30f0ca3c914ef00342e
Matt <mwollam>
2019-02-22 00:11:27 -0500

    Winbuntu now installs to Program Files (x86) instead of wherever the user downloaded the file

--- installer.ps1
+++ installer.ps1
@@ -4,27 +4,34 @@
 # Global Variables              
 $start_time = Get-Date            
 $prog_loc = (Resolve-Path .\).Path
+$install_loc = "C:\Program Files (x86)\Winbuntu"
 $userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-$WSL = (Get-WindowsOptionalFeature -Online | where FeatureName -eq Microsoft-Windows-Subsystem-Linux)
+$WSL = (Get-WindowsOptionalFeature -Online | Where-Object FeatureName -eq Microsoft-Windows-Subsystem-Linux)
 if ($WSL.State -eq "Disabled") {
     Write-Host "Please install Windows Subsystem for Linux"
     exit
 }
 
+# Creating the home directory for Winbuntu
+if (Test-Path $install_loc) {
+    Write-Host "Installation location already exists, please remove '$install_loc' and restart the installer."
+    exit
+} else { Invoke-Expression "mkdir '$install_loc'" }
 
 # Downloads the ubuntu client
 Write-Host "Downloading and extracting Ubuntu terminal"
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile Ubuntu.appx -UseBasicParsing
+$ubuntu = "$install_loc\Ubuntu.appx"
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile $ubuntu -UseBasicParsing
 Rename-Item ~/Ubuntu.appx Ubuntu.zip
 Expand-Archive Ubuntu.zip Ubuntu
 
 # Adds the ubuntu application to your path
 Write-Host "Adding Ubuntu to path"
-$addPath = [System.Environment]::SetEnvironmentVariable("PATH", $userenv + "C:\Users\Administrator\Ubuntu", "User")
+$addPath = [System.Environment]::SetEnvironmentVariable("PATH", $userenv + $install_loc, "User")
 Invoke-Expression -Command $addPath
 
 # Finally, restarts the computer so changes take effect
 Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-#Write-Host "Restarting computer to save changes"
+Write-Host "Restarting computer to save changes"
 #Restart-Computer   

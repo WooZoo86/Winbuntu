0387029627f8c7cf291a557b74a9fa876f34ddfd
Matt <testtest@test.com>
2019-05-16 19:04:24 -0400

    Winbuntu now checks if the program is installed on the path

--- src/RegEdit.cs
+++ src/RegEdit.cs
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Winbuntu.  If not, see <https://www.gnu.org/licenses/>.
 */
+using System;
+using System.IO;
 
 namespace Winbuntu
 {
@@ -21,12 +23,39 @@ namespace Winbuntu
     {
         public static void Add()
         {
-
+            string target = Program._args[Program._args.Count - 1];
+            string targetPath = FindExePath(target);
+            Console.WriteLine("Installing {0} at {1}", target, targetPath);
         }
 
         public static void Remove()
         {
             
+        }
+
+        /// <summary>
+        /// Determines if the exe is on the system path or the current working directory
+        /// </summary>
+        /// <param name="exe"> The name of the executable file </param>
+        /// <returns> The full path of the executable </returns>
+        public static string FindExePath(string exe)
+        {
+            exe = Environment.ExpandEnvironmentVariables(exe);
+            if (!File.Exists(exe))
+            {
+                if (Path.GetDirectoryName(exe) == String.Empty)
+                {
+                    foreach (string test in (Environment.GetEnvironmentVariable("PATH") ?? "").Split(';'))
+                    {
+                        string path = test.Trim();
+                        if (!String.IsNullOrEmpty(path) && File.Exists(path = Path.Combine(path, exe)))
+                        return Path.GetFullPath(path);
+                     }
+                }
+                Console.WriteLine("{0} is not on the path, please try another program or direct path.", exe);
+                Environment.Exit(0);
+            }
+            return Path.GetFullPath(exe);
         }
         
     }
